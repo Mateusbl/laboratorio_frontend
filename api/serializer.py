4fc0e609dee735d5b7608b3bffc6a1294e7b32ee
Mateus Barbosa <mateusbarbosa999@gmail.com>
2024-11-07 21:20:00 -0300

    Add password validation and user creation logic to RegisterSerializer

--- api/serializer.py
+++ api/serializer.py
@@ -28,4 +28,18 @@ class RegisterSerializer(serializers.ModelSerializer):
          fields =  ['email', 'username', 'password', 'password2']
 
 
-         def validate
+         def validate(self, attrs):
+            if attrs['password'] != attrs['password2']:
+                raise serializers.ValidationError({"password": "Password fields didn't match."})
+
+            return attrs
+        def create(self, validated_data):
+            user = User.objects.create(
+                email   =validated_data['email'],
+                username=validated_data['username'],
+            )
+
+            user.set_password(validated_data['password'])
+            user.save()
+
+            return user
