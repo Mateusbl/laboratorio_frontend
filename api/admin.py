a6f7ecd78797769e47de8438c8b5a25710d14e57
Mateus Barbosa <mateusbarbosa999@gmail.com>
2024-11-07 21:14:21 -0300

    Refactor admin configurations and add initial migrations for User and Profile models

--- api/admin.py
+++ api/admin.py
@@ -1,18 +1,7 @@
 from django.contrib import admin
 from .models import User, Profile
 class UserAdmin(admin.ModelAdmin):
-    list_display = ('id', 'username', 'email', 'is_staff', 'is_active', 'date_joined')
-    list_display_links = ('id', 'username')
-    search_fields = ('username', 'email')
-    list_per_page = 25
-
-    class ProfileAdmin(admin.ModelAdmin):
-        list_display = ('id', 'user', 'bio', 'location', 'birth_date')
-        list_display_links = ('id', 'user')
-        search_fields = ('user', 'bio', 'location', 'birth_date')
-        list_editable = ['user', 'full_name', 'verified']
-        list_per_page = 25
-
-    admin.site.register(User, UserAdmin)
-    admin.site.register(Profile, ProfileAdmin)
-# Register your models here.
+    list_display = ['username', 'email', ]
+class ProfileAdmin(admin.ModelAdmin):
+    list_editable = ['verified', ]
+    list_display = ['user', 'full_name', 'verified', ]
